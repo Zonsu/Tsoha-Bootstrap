8fd29e6d0c37eb77c3ef5b7eedd8f9bdf8d72d9a
Zonsu <johanna.paavola@helsinki.fi>
2015-08-04 18:48:51 +0300

    Service all()

--- app/models/Service.php
+++ app/models/Service.php
@@ -1,10 +1,37 @@
 <?php
 
 /**
- * Luokka kannassa oleville palveluille.
+ * Luokka kannassa oleville palveluille. Palvelulla on nimi, hinta, kuvaus sekä
+ *  sitä tarjoavat työntekijät jotka löytyvät offered_services taulusta.
  *
  * @author johanna
  */
-class Service {
-    //put your code here
+class Service extends BaseModel {
+
+    public $id, $name, $price, $description;
+
+    public function __construct($attributes) {
+        parent::__construct($attributes);
+    }
+
+    public static function all() {
+
+        $query = DB::connection()->prepare('SELECT * FROM Service');
+
+        $query->execute();
+
+        $rows = $query->fetchAll();
+        $services = array();
+
+        foreach ($rows as $row) {
+            $services[] = new Service(array(
+                'id' => $row['id'],
+                'name' => $row['name'],
+                'price' => $row['price'],
+                'description' => $row['description']
+            ));
+        }
+        return $services;
+    }
+
 }
