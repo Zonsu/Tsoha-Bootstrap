ac0070fb38ccc0b647bfe6f49bebdb9223f520c5
Zonsu <johanna.paavola@helsinki.fi>
2015-08-04 03:07:31 +0300

    monta taulua ja testiä

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,11 +1,30 @@
-INSERT INTO Employee (name, startingDate, special, introduction) VALUES ('Jack Bauer', '2013-04-01', 'cool dude', 'Ei koskaan anna periksi');
+INSERT INTO Employee (name, startingDate, special, introduction) VALUES ('Jack Bauer', '2013-04-01', 'Parturipalvelut', 'Ei koskaan anna periksi');
+INSERT INTO Employee (name, startingDate, special, introduction) VALUES ('Elmo', '2011-06-22', 'Värjäys', 'Pehmo');
+INSERT INTO Employee (name, startingDate, special, introduction) VALUES ('Jossu', '2015-07-03', 'Opiskelijatyöt', 'Noobi');
 INSERT INTO Employee (name, startingDate, special, introduction) VALUES ('Elmo', '2011-06-22', 'Värjäys', 'Pehmo');
 
-INSERT INTO Service (name, price, description) VALUES ('Hiustenleikkuu - Lyhyet', '20', 'Lyhyiden hiusten leikkuu ja mallin palautus');
+
+INSERT INTO Service (name, price, description) VALUES ('Hiustenleikkuu - Lyhyet', '15', 'Lyhyiden hiusten leikkuu ja mallin palautus');
+INSERT INTO Service (name, price, description) VALUES ('Hiustenleikkuu - Keskipitkät', '30', 'Keskipitkien hiusten leikkuu ja mallin palautus');
+INSERT INTO Service (name, price, description) VALUES ('Hiustenleikkuu - Lyhyet', '40', 'Pitkien hiusten leikkuu ja mallin palautus');
+INSERT INTO Service (name, price, description) VALUES ('Ajo/Otsatukan tasaus', '10', 'Pään ajo koneella tai otsatukan tasaus.');
 
 INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('1', '1');
+INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('1', '2');
+INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('1', '3');
+INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('1', '4');
 INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('2', '1');
+INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('2', '2');
+INSERT INTO Offered_Services (employeeID, serviceID) VALUES ('4', '4');
 
 DELETE FROM Employee WHERE id = 2;
+
+INSERT INTO Client (firstName, lastName, email, phoneNumber) VALUES ('pöllö', 'pöllönen', 'asd@asd.asd', '12312313');
+
+INSERT INTO Workshift (date, start, endtime, employeeID) VALUES ('2015-08-04','08:00:00','18:00:00','3');
+
+INSERT INTO Reservation (clientID, serviceID, employeeID, date, start, endtime, message) VALUES ('1', '2', '3','2015-08-04','12:00:00','13:00:00','HIRVEET TÖYHTÖT HALUUN');
+
+INSERT INTO Access (employeeId, access) VALUES ('3', 'true'); 
 
 
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,3 +1,4 @@
+
 CREATE TABLE Employee(
 id SERIAL PRIMARY KEY,
 name varchar(30) NOT NULL,
@@ -14,8 +15,44 @@ description varchar(500)
 );
 
 CREATE TABLE Offered_Services(
-serviceID integer REFERENCES Service,
-employeeID integer REFERENCES Employee,
+serviceID integer REFERENCES Service ON DELETE CASCADE,
+employeeID integer REFERENCES Employee ON DELETE CASCADE,
 PRIMARY KEY (serviceID, employeeID)
-); 
+);
+
+CREATE TABLE Client(
+ID SERIAL PRIMARY KEY,
+firstName varchar(30) NOT NULL,
+lastName varchar(30) NOT NULL,
+email varchar(30) NOT NULL,
+phoneNumber varchar(20) NOT NULL,
+username varchar(20),
+password varchar(50)
+);
+
+CREATE TABLE Workshift(
+id SERIAL PRIMARY KEY,
+date DATE NOT NULL,
+start TIME NOT NULL,
+endtime TIME NOT NULL,
+employeeID Integer REFERENCES Employee ON DELETE CASCADE
+);
+
+CREATE TABLE Reservation(
+id SERIAL PRIMARY KEY,
+clientID integer REFERENCES Client NOT NULL,
+serviceID integer REFERENCES Service DEFAULT 1,
+employeeID integer REFERENCES Employee NOT NULL,
+date DATE NOT NULL,
+start TIME NOT NULL,
+endtime TIME NOT NULL,
+message varchar(1000)
+);
+
+
+CREATE TABLE Access(
+id SERIAL PRIMARY KEY,
+employeeID Integer REFERENCES Employee ON DELETE CASCADE,
+access boolean DEFAULT FALSE
+);
 
